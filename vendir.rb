957742a163a3af48541c07ef6ba45d4e598651f6
Dmitriy Kalinin <cppforlife@gmail.com>
2021-02-03 11:55:34 -0500

    Update vendir.rb

--- vendir.rb
+++ vendir.rb
@@ -1,14 +1,14 @@
 class Vendir < Formula
   desc "Declaratively state what should be in a directory"
-  homepage "https://github.com/k14s/vendir"
-  version "v0.13.0"
+  homepage "https://carvel.dev/vendir"
+  version "v0.16.0"
 
   if OS.mac?
-    url "https://github.com/k14s/vendir/releases/download/v0.13.0/vendir-darwin-amd64"
-    sha256 "5901dba6c3b73ec16f64cd13ffb731df0f8f6ff05369d184187a60aa3f76c662"
+    url "https://github.com/k14s/vendir/releases/download/v0.16.0/vendir-darwin-amd64"
+    sha256 "3e6af7ae5cd89579f6d153af6b6a4c0ab1cfcac22f5014b983d1d942feb8bab0"
   elsif OS.linux?
-    url "https://github.com/k14s/vendir/releases/download/v0.13.0/vendir-linux-amd64"
-    sha256 "d779c002059f7dccc8d3d83c4e703b6fd6eb7748acb26da7e0210261e61ae727"
+    url "https://github.com/k14s/vendir/releases/download/v0.16.0/vendir-linux-amd64"
+    sha256 "05cede475c2b947772a9fe552380927054d48158959c530122a150a93bf542dd"
   end
 
   depends_on :arch => :x86_64
