39131503319ca945982f04482075bafbc8c6a760
Cari <carilynndean@gmail.com>
2021-03-09 15:55:52 -0800

    Bump kapp version to v0.36.0

--- kapp.rb
+++ kapp.rb
@@ -1,14 +1,14 @@
 class Kapp < Formula
   desc "Kapp"
   homepage "https://get-kapp.io"
-  version "v0.35.0"
+  version "v0.36.0"
 
   if OS.mac?
-    url "https://github.com/k14s/kapp/releases/download/v0.35.0/kapp-darwin-amd64"
-    sha256 "2f7b37c9cf5a8fd46e7b5da181c93ac3c16fba21664e3c5e413e38fca3510cc2"
+    url "https://github.com/k14s/kapp/releases/download/v0.36.0/kapp-darwin-amd64"
+    sha256 "1076da52c7291a7b9c7c0169b59fa18ef5248dee1e060676f8f5af325826d508"
   elsif OS.linux?
-    url "https://github.com/k14s/kapp/releases/download/v0.35.0/kapp-linux-amd64"
-    sha256 "0f9d4daa8c833a8e245362c77e72f4ed06d4f0a12eed6c09813c87a992201676"
+    url "https://github.com/k14s/kapp/releases/download/v0.36.0/kapp-linux-amd64"
+    sha256 "22fe308f1d9ebbb829a6ea10ca80d9468ff4b9aa911b0c33788fe67d04ccb383"
   end
 
   depends_on :arch => :x86_64
