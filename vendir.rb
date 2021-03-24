18511be67baf33b44a215052a4b23f8a808665d6
Dakota Chambers <dakotachambers@gmail.com>
2021-03-24 11:11:37 -0500

    Change download URL to updated source and version 0.17.0

--- vendir.rb
+++ vendir.rb
@@ -4,10 +4,10 @@ class Vendir < Formula
   version "v0.17.0"
 
   if OS.mac?
-    url "https://github.com/k14s/vendir/releases/download/v0.16.0/vendir-darwin-amd64"
+    url "https://github.com/vmware-tanzu/carvel-vendir/releases/download/v0.17.0/vendir-darwin-amd64"
     sha256 "695baed3f339cb0f3e0354e80e46677539d45511c2fb09c7616af93aac49cc1b"
   elsif OS.linux?
-    url "https://github.com/k14s/vendir/releases/download/v0.16.0/vendir-linux-amd64"
+    url "https://github.com/vmware-tanzu/carvel-vendir/releases/download/v0.17.0/vendir-linux-amd64"
     sha256 "93958682953a0e6f1f403b67a5df9507a070b964b0c1c961eedae0b25edf4df0"
   end
 
