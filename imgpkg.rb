f85a0b32706c8bfcbf08f8af6c256aa5df0c2517
Dennis Leon <leonde@vmware.com>
2021-04-15 15:00:27 -0700

    Fix imgpkg sha256
    Authored-by: Dennis Leon <leonde@vmware.com>

--- imgpkg.rb
+++ imgpkg.rb
@@ -5,10 +5,10 @@ class Imgpkg < Formula
 
   if OS.mac?
     url "https://github.com/vmware-tanzu/carvel-imgpkg/releases/download/v0.6.1/imgpkg-darwin-amd64"
-    sha256 "https://github.com/vmware-tanzu/carvel-imgpkg/releases/download/v0.6.1/imgpkg-darwin-amd64"
+    sha256 "832e7425272f67fbabc51633eae6612927e9ddf317d752cf21d3f9dd09163138"
   elsif OS.linux?
     url "https://github.com/vmware-tanzu/carvel-imgpkg/releases/download/v0.6.1/imgpkg-linux-amd64"
-    sha256 "https://github.com/vmware-tanzu/carvel-imgpkg/releases/download/v0.6.1/imgpkg-linux-amd64"
+    sha256 "67a368333528bedbe0b951e045186e7be62a1dd7600b7f32e9f662079ad34f0c"
   end
 
   depends_on :arch => :x86_64
