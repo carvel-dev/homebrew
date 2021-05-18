f234644a2bedd1d02879f377d32effa788641d5b
João Pereira <joaod@vmware.com>
2021-05-18 10:20:35 -0400

    Bump kbld to 0.30.0

--- kbld.rb
+++ kbld.rb
@@ -1,14 +1,14 @@
 class Kbld < Formula
   desc "kbld helps working with container images used in Kubernetes configuration files"
   homepage "https://carvel.dev/kbld/"
-  version "v0.29.0"
+  version "v0.30.0"
 
   if OS.mac?
-    url "https://github.com/vmware-tanzu/carvel-kbld/releases/download/v0.29.0/kbld-darwin-amd64"
-    sha256 "3754fa1ba3a26368aef32769bfb084dd62e52be9c5af0b65e574729e92d21cbc"
+    url "https://github.com/vmware-tanzu/carvel-kbld/releases/download/v0.30.0/kbld-darwin-amd64"
+    sha256 "73274d02b0c2837d897c463f820f2c8192e8c3f63fd90c526de5f23d4c6bdec4"
   elsif OS.linux?
-    url "https://github.com/vmware-tanzu/carvel-kbld/releases/download/v0.29.0/kbld-linux-amd64"
-    sha256 "28492a398854e8fec7dd9537243b07af7f43e6598e1e4557312f5481f6840499"
+    url "https://github.com/vmware-tanzu/carvel-kbld/releases/download/v0.30.0/kbld-linux-amd64"
+    sha256 "76c5c572e7a9095256b4c3ae2e076c370ef70ce9ff4eb138662f56828889a00c"
   end
 
   depends_on :arch => :x86_64
