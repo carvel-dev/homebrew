c5ed1142b93e0482cb75e500ac0000172f1883c5
João Pereira <joaod@vmware.com>
2021-06-04 17:42:32 -0400

    update imgpkg version to 0.9.0

--- imgpkg.rb
+++ imgpkg.rb
@@ -1,14 +1,14 @@
 class Imgpkg < Formula
   desc "Imgpkg"
   homepage "https://carvel.dev/imgpkg/"
-  version "v0.8.0"
+  version "v0.9.0"
 
   if OS.mac?
-    url "https://github.com/vmware-tanzu/carvel-imgpkg/releases/download/v0.8.0/imgpkg-darwin-amd64"
-    sha256 "11439505cec7cd0c28182cdb2e269368be8c51376fdd57846289a4ad81460fde"
+    url "https://github.com/vmware-tanzu/carvel-imgpkg/releases/download/v0.9.0/imgpkg-darwin-amd64"
+    sha256 "bc6f038a7f482ef909007552538e06422ece3db377aac1b6c8e898fca355d716"
   elsif OS.linux?
-    url "https://github.com/vmware-tanzu/carvel-imgpkg/releases/download/v0.8.0/imgpkg-linux-amd64"
-    sha256 "d998c1628c5956ffc84b36f23bec6fd0145977e76f3c02a0c28962e6f8f233c2"
+    url "https://github.com/vmware-tanzu/carvel-imgpkg/releases/download/v0.9.0/imgpkg-linux-amd64"
+    sha256 "db3e8eef266cb3c98e1cc58a574145f5246ea3cbee27f4539e98b699ae0b694d"
   end
 
   depends_on :arch => :x86_64
