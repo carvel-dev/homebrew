5e9104430d661701259079e203811b60e42ade08
Cari <cdean@pivotal.io>
2021-07-12 14:58:36 -0700

    Update ytt shasums to match release
    - We found a missing step in our release process that excluded the `ytt
      website` command so we updated the release to fix this.

--- ytt.rb
+++ ytt.rb
@@ -6,20 +6,20 @@ class Ytt < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/vmware-tanzu/carvel-ytt/releases/download/v0.35.0/ytt-darwin-arm64"
-      sha256 "faac1c8595ca5e3b1b49add19692d07db2670cdaca72ec5a6d79eadd7b9cd5eb"
+      sha256 "3304f422c7192ea20790ae5ad8246d8310dbe1766901a73fe72b9a8ba32b982c"
     else
       url "https://github.com/vmware-tanzu/carvel-ytt/releases/download/v0.35.0/ytt-darwin-amd64"
-      sha256 "1c8d5c28c107da2767d1688fd3aaff86ba9d8f26706d12b0986d49ada24f39f2"
+      sha256 "425c5a66c6d1cb765fff11b670081dbe9c63679d25129e4f566f37242cef324d"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/vmware-tanzu/carvel-ytt/releases/download/v0.35.0/ytt-linux-arm64"
-      sha256 "f1a8fd1f2b1e45384590b50c90513347bd77772967c99feb3372a232abc14abe"
+      sha256 "11ed09e6b842029f47cea376c7f0c54110f3a015ae41a2c8a505d448cb69019a"
     else
       url "https://github.com/vmware-tanzu/carvel-ytt/releases/download/v0.35.0/ytt-linux-amd64"
-      sha256 "1d129457872d777c14c187ecc40b99c49d9206502af0b9c29bbee68897a1e5d9"
+      sha256 "23718f9b92b952f53b8ee603981031e3d2a54d9ad2aaf933da692d8e07a33c90"
     end
   end
 
