09c96b5a17ddd2d53f7437e661374766b9dc7268
Dennis Leon <leonde@vmware.com>
2021-07-13 16:02:50 -0700

    Bump imgpkg to 0.14.0
    Authored-by: Dennis Leon <leonde@vmware.com>

--- imgpkg.rb
+++ imgpkg.rb
@@ -1,14 +1,14 @@
 class Imgpkg < Formula
   desc "Imgpkg"
   homepage "https://carvel.dev/imgpkg/"
-  version "v0.13.0"
+  version "v0.14.0"
 
   if OS.mac?
-    url "https://github.com/vmware-tanzu/carvel-imgpkg/releases/download/v0.13.0/imgpkg-darwin-amd64"
-    sha256 "6b88505084f026cbc4f3cd7984ca753f47dd392e5e8859695113958cc1381c10"
+    url "https://github.com/vmware-tanzu/carvel-imgpkg/releases/download/v0.14.0/imgpkg-darwin-amd64"
+    sha256 "3c63f224833590526c3b48ab5db1be4ec07ece6a6eb4879888fefba14b6176be"
   elsif OS.linux?
-    url "https://github.com/vmware-tanzu/carvel-imgpkg/releases/download/v0.13.0/imgpkg-linux-amd64"
-    sha256 "98856613274f83364ab3156f5f6922455eefdf2302b26ed72e8f7ab0eb6ff82f"
+    url "https://github.com/vmware-tanzu/carvel-imgpkg/releases/download/v0.14.0/imgpkg-linux-amd64"
+    sha256 "bd53355fc3a05666681ddf2ba1dfae2da894bc1c74d86cdc545d772749abc887"
   end
 
   depends_on :arch => :x86_64
