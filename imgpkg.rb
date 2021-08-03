d8babd5c63ee92fda7f2dc6c72e0de035911158b
Dmitriy Kalinin <cppforlife@gmail.com>
2021-08-03 12:34:43 -0400

    Update imgpkg.rb

--- imgpkg.rb
+++ imgpkg.rb
@@ -1,14 +1,14 @@
 class Imgpkg < Formula
   desc "Imgpkg"
   homepage "https://carvel.dev/imgpkg/"
-  version "v0.16.0"
+  version "v0.17.0"
 
   if OS.mac?
-    url "https://github.com/vmware-tanzu/carvel-imgpkg/releases/download/v0.16.0/imgpkg-darwin-amd64"
-    sha256 "23bf8d161e9852a4f929568aee6afd3aea5b287d2958370f18877c9970e8ced3"
+    url "https://github.com/vmware-tanzu/carvel-imgpkg/releases/download/v0.17.0/imgpkg-darwin-amd64"
+    sha256 "f7b22603d887286f63cf858316932829cf99e6acfa3ad8962f63017aecccf52a"
   elsif OS.linux?
-    url "https://github.com/vmware-tanzu/carvel-imgpkg/releases/download/v0.16.0/imgpkg-linux-amd64"
-    sha256 "7668475a2b0be3bc3dd475312a4c8a19e650decaa61f10f5f62a6aafe6541473"
+    url "https://github.com/vmware-tanzu/carvel-imgpkg/releases/download/v0.17.0/imgpkg-linux-amd64"
+    sha256 "d54437b974ffef9aa5d6f913c9a5a75f02d6151e99ad5d72bfb96ecb2d17e58e"
   end
 
   depends_on :arch => :x86_64
