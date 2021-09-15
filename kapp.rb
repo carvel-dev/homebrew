8a1f382f751b6d9ba62bc2db7207f7a6934e7e25
rohitagg2020 <55523204+rohitagg2020@users.noreply.github.com>
2021-09-15 20:11:51 +0530

    Update kapp.rb

--- kapp.rb
+++ kapp.rb
@@ -1,25 +1,25 @@
 class Kapp < Formula
   desc "Kapp"
   homepage "https://get-kapp.io"
-  version "v0.39.0"
+  version "v0.40.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/k14s/kapp/releases/download/v0.39.0/kapp-darwin-arm64"
-      sha256 "dfd5d32c845b62fd2617342424fb4376ce9a5a6fe644a9bbdf4b9f31856533aa"
+      url "https://github.com/k14s/kapp/releases/download/v0.40.0/kapp-darwin-arm64"
+      sha256 "1632b8365e9392c08425e9ff8003fe5ff616b622c6e0255ef69bc87db0349afe"
     else
-      url "https://github.com/k14s/kapp/releases/download/v0.39.0/kapp-darwin-amd64"
-      sha256 "43d79433d3d4dad4ffde7c775ea99f5c6b10c8949d54fd2a048ba66aaea89a6b"
+      url "https://github.com/k14s/kapp/releases/download/v0.40.0/kapp-darwin-amd64"
+      sha256 "d667c5cd05eb20df74763b59f31203569f7c89f185c5b7945d74a1235c15040d"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/k14s/kapp/releases/download/v0.39.0/kapp-linux-arm64"
-      sha256 "e89b2bcdf42edf04b33bf57f3c033b146ecd763a52d3d7ec99d501fa2f0ae423"
+      url "https://github.com/k14s/kapp/releases/download/v0.40.0/kapp-linux-arm64"
+      sha256 "5a79561d07d6ef37fec795b9956d250cf9a1e4b245bd14bc154a21d9ab811a4c"
     else
-      url "https://github.com/k14s/kapp/releases/download/v0.39.0/kapp-linux-amd64"
-      sha256 "2120a627a867e04840d6e0e473097894a3b74b54b62f231b8df3f8670c4e80a3"
+      url "https://github.com/k14s/kapp/releases/download/v0.40.0/kapp-linux-amd64"
+      sha256 "94cfdcf8a3aec521a68c4c2c569434684e71914d42296bc9fee8bfe912f63984"
     end
   end
 
