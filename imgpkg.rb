23f95b46cd1d4b3942478b1f3f7126df69cd478d
Dennis Leon <leonde@vmware.com>
2021-09-20 14:24:57 -0700

    Bump imgpkg to 0.18.0
    - update to include ARM binaries
    
    Authored-by: Dennis Leon <leonde@vmware.com>

--- imgpkg.rb
+++ imgpkg.rb
@@ -1,17 +1,20 @@
 class Imgpkg < Formula
   desc "Imgpkg"
   homepage "https://carvel.dev/imgpkg/"
-  version "v0.17.0"
+  version "v0.18.0"
 
   if OS.mac?
-    url "https://github.com/vmware-tanzu/carvel-imgpkg/releases/download/v0.17.0/imgpkg-darwin-amd64"
-    sha256 "f7b22603d887286f63cf858316932829cf99e6acfa3ad8962f63017aecccf52a"
+    if Hardware::CPU.arm?
+      url "https://github.com/vmware-tanzu/carvel-imgpkg/releases/download/v0.18.0/imgpkg-darwin-amd64"
+      sha256 "976aebc0eef995e95b391bf7fe36be82c50faa726c151bab2646002a1c042245"
+    else
+      url "https://github.com/vmware-tanzu/carvel-imgpkg/releases/download/v0.18.0/imgpkg-darwin-amd64"
+      sha256 "6ef71c549deefb1b9b798f31525610c4a7a562401f22b3bdf18e0cb769921d32"
+    end
   elsif OS.linux?
-    url "https://github.com/vmware-tanzu/carvel-imgpkg/releases/download/v0.17.0/imgpkg-linux-amd64"
-    sha256 "d54437b974ffef9aa5d6f913c9a5a75f02d6151e99ad5d72bfb96ecb2d17e58e"
+    url "https://github.com/vmware-tanzu/carvel-imgpkg/releases/download/v0.18.0/imgpkg-linux-amd64"
+    sha256 "7e401eab1fbbaad8044cf3d82ff09cb8ec7666444ef91da9591f202e8b2a8a67"
   end
-
-  depends_on :arch => :x86_64
 
   def install
     bin.install stable.url.split("/")[-1] => "imgpkg"
