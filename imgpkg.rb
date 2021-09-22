bb89bd1e24fefe50973c96d053af1585e2a59cd3
Dennis Leon <leonde@vmware.com>
2021-09-22 15:16:08 -0700

    Bump imgpkg to 0.19.0
    Authored-by: Dennis Leon <leonde@vmware.com>

--- imgpkg.rb
+++ imgpkg.rb
@@ -1,19 +1,19 @@
 class Imgpkg < Formula
   desc "Imgpkg"
   homepage "https://carvel.dev/imgpkg/"
-  version "v0.18.0"
+  version "v0.19.0"
 
   if OS.mac?
     if Hardware::CPU.arm?
-      url "https://github.com/vmware-tanzu/carvel-imgpkg/releases/download/v0.18.0/imgpkg-darwin-amd64"
-      sha256 "976aebc0eef995e95b391bf7fe36be82c50faa726c151bab2646002a1c042245"
+      url "https://github.com/vmware-tanzu/carvel-imgpkg/releases/download/v0.19.0/imgpkg-darwin-arm64"
+      sha256 "d338feb164a0f0fe035457be324d92782367271775030120751e0e515edd69d9"
     else
-      url "https://github.com/vmware-tanzu/carvel-imgpkg/releases/download/v0.18.0/imgpkg-darwin-amd64"
-      sha256 "6ef71c549deefb1b9b798f31525610c4a7a562401f22b3bdf18e0cb769921d32"
+      url "https://github.com/vmware-tanzu/carvel-imgpkg/releases/download/v0.19.0/imgpkg-darwin-amd64"
+      sha256 "a548abe7adfd4dc3f8c86d63a26eb03168fafdb4503b6296f196106db303f71b"
     end
   elsif OS.linux?
-    url "https://github.com/vmware-tanzu/carvel-imgpkg/releases/download/v0.18.0/imgpkg-linux-amd64"
-    sha256 "7e401eab1fbbaad8044cf3d82ff09cb8ec7666444ef91da9591f202e8b2a8a67"
+    url "https://github.com/vmware-tanzu/carvel-imgpkg/releases/download/v0.19.0/imgpkg-linux-amd64"
+    sha256 "3a0a8b081fe35651dfbadf4c1531fe919a28159c70f92a2de49d42fa98a571ce"
   end
 
   def install
