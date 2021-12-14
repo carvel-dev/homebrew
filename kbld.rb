eacf6aaf51d15b072fe01f6e3fcc1a68deed23a2
Scott Andrews <andrewssc@vmware.com>
2021-12-13 19:06:34 -0500

    Include kbld for arm64 with darwin and linux
    Signed-off-by: Scott Andrews <andrewssc@vmware.com>

--- kbld.rb
+++ kbld.rb
@@ -3,15 +3,25 @@ class Kbld < Formula
   homepage "https://carvel.dev/kbld/"
   version "v0.31.0"
 
-  if OS.mac?
-    url "https://github.com/vmware-tanzu/carvel-kbld/releases/download/v0.31.0/kbld-darwin-amd64"
-    sha256 "d3b0a30bf3a79bedeb25d8548a91254954b99cd4a0c03f3a810b331fc4d1f071"
-  elsif OS.linux?
-    url "https://github.com/vmware-tanzu/carvel-kbld/releases/download/v0.31.0/kbld-linux-amd64"
-    sha256 "ba0be56d9e74b067f3e659de0b79100b0b9df86a2e3e0e6ff533b1e019c22c23"
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/vmware-tanzu/carvel-kbld/releases/download/v0.31.0/kbld-darwin-arm64"
+      sha256 "7287efdd8ba1a58b0a6a3cf5c41a9c35ba12b8b30ac3e275f68c9640af857903"
+    else
+      url "https://github.com/vmware-tanzu/carvel-kbld/releases/download/v0.31.0/kbld-darwin-amd64"
+      sha256 "d3b0a30bf3a79bedeb25d8548a91254954b99cd4a0c03f3a810b331fc4d1f071"
+    end
   end
 
-  depends_on :arch => :x86_64
+  on_linux do
+    if Hardware::CPU.arm?
+      url "https://github.com/vmware-tanzu/carvel-kbld/releases/download/v0.31.0/kbld-linux-arm64"
+      sha256 "7c69efa53994c29d2a090c153a65d8d7f10aa89fa7493668135a613875c910ce"
+    else
+      url "https://github.com/vmware-tanzu/carvel-kbld/releases/download/v0.31.0/kbld-linux-amd64"
+      sha256 "ba0be56d9e74b067f3e659de0b79100b0b9df86a2e3e0e6ff533b1e019c22c23"
+    end
+  end
 
   def install
     bin.install stable.url.split("/")[-1] => "kbld"
