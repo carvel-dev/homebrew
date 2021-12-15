0bd8c3f2b654d6325dfded9218647e5f9c92d619
k8slt <klt@groups.vmware.com>
2021-12-15 20:05:25 +0000

    chore: autopublish 2021-12-15T20:05:25Z

--- kbld.rb
+++ kbld.rb
@@ -1,17 +1,25 @@
 class Kbld < Formula
-  desc "kbld helps working with container images used in Kubernetes configuration files"
+  desc "Kbld"
   homepage "https://carvel.dev/kbld/"
   version "v0.31.0"
 
   if OS.mac?
-    url "https://github.com/vmware-tanzu/carvel-kbld/releases/download/v0.31.0/kbld-darwin-amd64"
-    sha256 "d3b0a30bf3a79bedeb25d8548a91254954b99cd4a0c03f3a810b331fc4d1f071"
+    if Hardware::CPU.arm?
+      url "https://github.com/vmware-tanzu/carvel-kbld/releases/download/v0.31.0/kbld-darwin-arm64"
+      sha256 "7287efdd8ba1a58b0a6a3cf5c41a9c35ba12b8b30ac3e275f68c9640af857903"
+    else
+      url "https://github.com/vmware-tanzu/carvel-kbld/releases/download/v0.31.0/kbld-darwin-amd64"
+      sha256 "d3b0a30bf3a79bedeb25d8548a91254954b99cd4a0c03f3a810b331fc4d1f071"
+    end
   elsif OS.linux?
-    url "https://github.com/vmware-tanzu/carvel-kbld/releases/download/v0.31.0/kbld-linux-amd64"
-    sha256 "ba0be56d9e74b067f3e659de0b79100b0b9df86a2e3e0e6ff533b1e019c22c23"
+    if Hardware::CPU.arm?
+      url "https://github.com/vmware-tanzu/carvel-kbld/releases/download/v0.31.0/kbld-linux-arm64"
+      sha256 "7c69efa53994c29d2a090c153a65d8d7f10aa89fa7493668135a613875c910ce"
+    else
+      url "https://github.com/vmware-tanzu/carvel-kbld/releases/download/v0.31.0/kbld-linux-amd64"
+      sha256 "ba0be56d9e74b067f3e659de0b79100b0b9df86a2e3e0e6ff533b1e019c22c23"
+    end
   end
-
-  depends_on :arch => :x86_64
 
   def install
     bin.install stable.url.split("/")[-1] => "kbld"
